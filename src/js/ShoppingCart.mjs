a088a095087e277bac0ceb1406f46d466f9cd397
Ronaldo Campos <cam23008@byui.edu>
2025-03-26 18:23:00 -0600

    week 03 team activity

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,31 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
+
+}
+
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+            <a href="product_pages/?product=${product.Id}">
+              <img
+                src="${product.Image}"
+                alt="${product.Name}"
+              />
+              <h3 class="card__brand">${product.Brand.Name}</h3>
+              <h2 class="card__name">${product.Name}</h2>
+              <p class="product-card__price">$${product.FinalPrice}</p></a
+            >
+          </li>
+    `;
+}
