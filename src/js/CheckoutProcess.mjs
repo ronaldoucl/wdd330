877c13e13f786f7cda13f3aee847fa81e680f4e4
Ronaldo Campos <cam23008@byui.edu>
2025-03-30 20:06:29 -0600

    corrections

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,7 +1,33 @@
 import { getLocalStorage } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+
+const services = new ExternalServices();
+
+// takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+function packageItems(items) {
+  const simplifiedItems = items.map((item) => {
+    console.log(item);
+    return {
+      id: item.Id,
+      price: item.FinalPrice,
+      name: item.Name,
+      quantity: item.quantity,
+    };
+  });
+  return simplifiedItems;
+}
+
+function formDataToJSON(formElement) {
+  // convert the form data to a JSON object
+  const formData = new FormData(formElement);
+  const convertedJSON = {};
+  formData.forEach((value, key) => {
+    convertedJSON[key] = value;
+  });
+  return convertedJSON;
+}
 
 export default class CheckoutProcess {
-
   constructor(key, outputSelector) {
     this.key = key;
     this.outputSelector = outputSelector;
@@ -21,9 +47,26 @@ export default class CheckoutProcess {
     this.updateDOM();
   }
 
-  calculeSubtotal () {
+  async checkout() {
+    const formElement = document.forms["checkout"];
+    const order = formDataToJSON(formElement);
 
-    this.list.forEach(element => {
+    order.orderDate = new Date().toISOString();
+    order.orderTotal = this.orderTotal;
+    order.tax = this.tax;
+    order.shipping = this.shipping;
+    order.items = packageItems(this.list);
+
+    try {
+      const response = await services.checkout(order);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  calculeSubtotal() {
+    this.list.forEach((element) => {
       this.subtotal = this.subtotal + Number.parseInt(element.FinalPrice);
     });
   }
@@ -34,8 +77,8 @@ export default class CheckoutProcess {
 
   calculateShippingEstimate() {
     let length = this.list.length;
-    if(length > 0) {
-      this.shipping = 10 + (2 * (length - 1));
+    if (length > 0) {
+      this.shipping = 10 + 2 * (length - 1);
     }
   }
 
@@ -44,9 +87,14 @@ export default class CheckoutProcess {
   }
 
   updateDOM() {
-    document.querySelector(".checkout-summary #subtotal").textContent = "$" + this.subtotal.toFixed(2);
-    document.querySelector(".checkout-summary #tax").textContent = "$" + this.tax.toFixed(2);
-    document.querySelector(".checkout-summary #shipping").textContent = "$" + this.shipping.toFixed(2);
-    document.querySelector(".checkout-summary #order-total").textContent = "$" + this.orderTotal.toFixed(2);
+    document.querySelector(".checkout-summary #num-items").textContent = this.list.length;
+    document.querySelector(".checkout-summary #subtotal").textContent =
+      "$" + this.subtotal.toFixed(2);
+    document.querySelector(".checkout-summary #tax").textContent =
+      "$" + this.tax.toFixed(2);
+    document.querySelector(".checkout-summary #shipping").textContent =
+      "$" + this.shipping.toFixed(2);
+    document.querySelector(".checkout-summary #order-total").textContent =
+      "$" + this.orderTotal.toFixed(2);
   }
 }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -5,3 +5,10 @@ loadHeaderFooter();
 
 const checkOutInformation = new CheckoutProcess("so-cart", "checkout-summary");
 checkOutInformation.init();
+
+//buttom function
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  checkOutInformation.checkout();
+});
